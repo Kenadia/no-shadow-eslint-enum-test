a25d1db699ad1b62bca98bc49826112a3f36baf9
Kenneth Schiller <Kenadia@users.noreply.github.com>
2020-09-01 11:31:45 -0700

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-shadow": "error"
+    }
+};
